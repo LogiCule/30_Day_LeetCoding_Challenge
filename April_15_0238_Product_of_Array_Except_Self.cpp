979195f8fb55096fa454817a4ea7c094841f9878
Rahul Dubey <37729273+LogiCule@users.noreply.github.com>
2020-04-15 12:53:06 +0530

    Create April_15_0238_Product_of_Array_Except_Self.cpp

---
+++ April_15_0238_Product_of_Array_Except_Self.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int len=nums.size();
+        vector<int> left(len);
+        vector<int> right(len);
+        vector<int> ans(len);
+        left[0]=1;
+        right[len-1]=1;
+        for(int i=1;i<len;i++)
+        {
+            left[i]=left[i-1]*nums[i-1];
+        }
+        for(int i=len-2;i>=0;i--)
+        {
+            right[i]=right[i+1]*nums[i+1];
+        }
+        for(int i=0;i<len;i++)
+        {
+            ans[i]=left[i]*right[i];
+        }
+        return ans;
+    }
+};
