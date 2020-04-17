b8bdefc4971be2202328154e03e627622ca8c881
Rahul Dubey <37729273+LogiCule@users.noreply.github.com>
2020-04-17 13:47:07 +0530

    Update April_17_0200_Number_of_Islands.cpp

--- April_17_0200_Number_of_Islands.cpp
+++ April_17_0200_Number_of_Islands.cpp
@@ -26,3 +26,10 @@ public:
         return count;
     }
 };
+/*
+Explaination: This code has an interesting approach that yoiu may have seen in minesweeper and is used in computer graphics 
+thats why i want to explain it.
+This algo is like 4 point flood fill ,whenever you encounter a 1 you fill it with it with 0 
+and keep filling its neighbour with 0s till no neighbors with value of 1 remain.
+and increase your count of island by 1;
+*/
